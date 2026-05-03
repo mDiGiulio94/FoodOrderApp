c17f5267428a24273e17900daebbe6875c77fc5f
Marco Di Giulio <m.digiulio@jdk.it>
2026-05-03 15:44:38 +0200

    increase-decrease-quantity-modal

--- src/components/CartComponent.jsx
+++ src/components/CartComponent.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../utils/fomratting";
 import Buttons from "./UI/Buttons";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItems from "./CartItems";
 
 export default function Cart() {
   const cartContext = useContext(CartContext);
@@ -21,9 +22,14 @@ export default function Cart() {
       <h2>Cart</h2>
       <ul>
         {cartContext.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItems 
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartContext.addItem(item)}
+            onDecrease={() => cartContext.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">Total: {currencyFormatter.format(cartTotal)}</p>
